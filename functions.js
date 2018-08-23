d8598005ef07374298f9d750576c935b10bcfc7c
David W. Li <14daweii@gmail.com>
2018-08-23 13:52:53 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,94 @@
+// Button links to Bookmarks List
+	function goSalesforce() {var url = 'https://rubiconinternational.lightning.force.com'; window.open(url);}
+	function goCalendar() {var url = 'https://outlook.office.com/owa/?realm=rubicon.com&exsvurl=1&ll-cc=1033&modurl=0&path=/calendar/view/Month'; window.open(url);}
+	function goADP() {var url = 'https://workforcenow.adp.com/public/login/wfnlogin.fcc?TYPE=33554433&REALMOID=06-7521e154-afdc-101f-ae21-844af8f80cb3&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-6FRTWl8QLZbL0pPTpuxvhgWB6tHbcXeDdpUAS2u%2bQtf1KhoIcl8artRxWa1CiElt&TARGET=-SM-https%3a%2f%2fworkforcenow%2eadp%2ecom%2fportal%2ftheme'; window.open(url);}
+	function goExpensify() {var url = 'https://www.expensify.com/signin?exitTo=%2Finbox&message=expired&email=dli%40rubicon.com'; window.open(url);}
+	function goIntranet() {var url = 'https://fs.rubicon.com/intranet/'; window.open(url);}
+	function goJobs() {var url = 'https://fs.rubicon.com/intranet/hr/#careers'; window.open(url);}
+	function goServices() {var url = 'https://fs.rubicon.com/intranet/services-support/'; window.open(url);}
+	function goFlightSchool() {var url = 'https://fs.rubicon.com/i/adminsite/client_search.php'; window.open(url);}
+
+
+// Copy Password
+	function goPassword() {
+		var copyText = document.getElementById("myInput"); copyText.select(); document.execCommand("copy");
+		var snackbar = document.getElementById("snackbar"); snackbar.className = "show";
+		setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 2000);
+	}
+
+
+// Button links to SAS List
+	function goDistrict() {var url = 'https://airtable.com/shrzt2THZ2WEJ1Exu'; window.open(url);}
+	function goSAS() {var url = 'https://airtable.com/tblOTEoLWRzebB0cN/viwPHbZ1NakhKkALm'; window.open(url);}
+
+
+// Button links to Deployment List
+	function goAddProject() {var url = 'https://airtable.com/shrqGJMd0DJme90da'; window.open(url);}
+	function goAddClient() {var url = 'https://airtable.com/shrbbs4WXvvglYJOW'; window.open(url);}
+	function goAddReplyall() {var url = 'https://airtable.com/shrTTkzuVoaAR0Q5g'; window.open(url);}
+	function goClipboard() {var url = 'https://airtable.com/tblERSvHhGiCqCRU9/viwwCA4N3VxliKaFx'; window.open(url);}
+	function goSuperAdmin() {var url = 'https://airtable.com/tblKgfzzI39CEnqNI/viwCQiBNhq0ky8Wyh'; window.open(url);}
+	
+
+// Button links to Card - Standards
+	function goStandards() {var url = 'https://airtable.com/shrZ91q5ORE3WM8jl'; window.open(url);}
+	function goScripts() {var url = 'StandardsScripts.html'; window.open(url);}
+	function goRegEx() {var url = 'https://airtable.com/shrzDPrkPirbvOHWW'; window.open(url);}
+	function goLatitude() {var url = 'https://fs.rubicon.com/Internal/Standards/Uploader/View/Default'; window.open(url);}
+	function goSweep() {var url = 'https://airtable.com/shr9S9EumzuHKyE8T'; window.open(url);}
+	function goMessage() {var url = 'https://fs.rubicon.com/internal/FlightSchool/RubiconChannel'; window.open(url);}
+	function goRecommendation() {var url = 'https://airtable.com/shrfVOjegL73yewV0'; window.open(url);}
+	function goFlags() {var url = 'https://airtable.com/shrbFGkZ8NkeDPgMb'; window.open(url);}
+
+
+
+// Button links to Card - Configuration
+	function goLocalization() {var url = 'https://fs.rubicon.com/Internal/LocalizationEditor/View/Default'; window.open(url);}
+	function goViewonly() {var url = 'https://fs.rubicon.com/i/adminsite/dev_unitmaps.php'; window.open(url);}
+	function goConstants() {var url = 'https://airtable.com/shrxP6L1ihdzHmGJk'; window.open(url);}
+	function goWidgets() {var url = 'WidgetSetup.html'; window.open(url);}
+	function goSitetoSite() {var url = 'https://fs.rubicon.com/i/adminsite/newsitesitecopy.php'; window.open(url);}
+	function goBugs() {var url = 'https://fs.rubicon.com/Internal/Development/Bugs/View/Default'; window.open(url);}
+	function goSuggestions() {var url = 'https://fs.rubicon.com/intranet/products/#1523554211974-8e6fb768-8bec'; window.open(url);}
+
+
+
+// Button links to Card - Site Build
+	function goAtlas() {var url = 'https://airtable.com/shryoP1L7OEVoz5iC'; window.open(url);}
+	function goLooknFeel() {var url = 'https://airtable.com/shrRpU1oMySfuMp7Z'; window.open(url);}
+	function goState() {var url = 'https://airtable.com/shrUes5hSh5MlrTyU'; window.open(url);}
+	function goSiteBuild() {var url = ''; window.open(url);}
+
+
+
+// Button links to Card - Shell Systems
+	function goUbD() {var url = 'https://fs.rubicon.com/i/adminsite/client_entry.php?ClientID=3407&'; window.open(url);}
+	function goPDT() {var url = 'https://fs.rubicon.com/i/adminsite/client_entry.php?ClientID=6333&'; window.open(url);}
+	function goIB() {var url = 'https://fs.rubicon.com/i/adminsite/client_entry.php?ClientID=5123&'; window.open(url);}
+	function goAU() {var url = 'https://fs.rubicon.com/i/adminsite/client_entry.php?ClientID=4073&'; window.open(url);}
+	function goNYC() {var url = 'https://fs.rubicon.com/i/adminsite/client_entry.php?ClientID=5482&'; window.open(url);}
+	function goENY() {var url = 'https://fs.rubicon.com/i/adminsite/client_entry.php?ClientID=5170&'; window.open(url);}
+	function goECCSS() {var url = 'https://fs.rubicon.com/i/adminsite/client_entry.php?ClientID=5659&'; window.open(url);}
+	function goOS() {var url = 'https://fs.rubicon.com/i/adminsite/client_entry.php?ClientID=5616&'; window.open(url);}
+
+
+
+// Button links to Card - Resources
+	function goProjects() {var url = 'https://airtable.com/shrCzHkzuk1zYg3hv'; window.open(url);}
+	function goHogwarts() {var url = 'https://fs.rubicon.com/i/adminsite/client_entry.php?ClientID=4085&'; window.open(url);}
+	function goCancellations() {var url = 'https://airtable.com/shryF6rS27k9XPFRP'; window.open(url);}
+	function goHoroscope() {var url = 'https://airtable.com/shrzo16g5Ksr791EM'; window.open(url);}
+	function goGallup() {var url = 'https://airtable.com/shrF52V8cOwzEN3lI'; window.open(url);}
+	function goReplyall() {var url = 'https://airtable.com/shrjQMi4G9nuNlVge'; window.open(url);}
+
+
+
+// Tab Links
+	function openTAB(evt, tabName) {
+		var i, tabContent, tabLink; 
+		tabContent = document.getElementsByClassName("tabContent"); 
+		for (i = 0; i < tabContent.length; i++) {tabContent[i].style.display = "none";}
+		tabLink = document.getElementsByClassName("tabLink");
+		for (i = 0; i < tabLink.length; i++) {tabLink[i].className = tabLink[i].className.replace(" active", "");}
+		document.getElementById(tabName).style.display = "block"; evt.currentTarget.className += " active";
+	} document.getElementById("defaultOpen").click();
